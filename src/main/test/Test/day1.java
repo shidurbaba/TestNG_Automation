ea4f26fee7629be40e93bc098f1d854d25444410
shidurbaba <shidurbaba@gmail.com>
2023-02-06 08:48:11 -0500

    update folder

---
+++ src/main/test/Test/day1.java
@@ -0,0 +1,14 @@
+package Test;
+
+import org.testng.annotations.Test;
+
+public class day1 {
+
+    @Test
+    public void Demo()
+    {
+        System.out.println("testing Test.day1");
+    }
+
+
+}
